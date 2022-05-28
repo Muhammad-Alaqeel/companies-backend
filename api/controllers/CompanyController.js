fbe6456e93eecdbaa59774d7caf0252d4703bec4
Mouhammed-Alaqeel <muhammed_alaqeel@hotmail.com>
2022-05-29 00:12:43 +0300

    Initial commit

---
+++ api/controllers/CompanyController.js
@@ -0,0 +1,72 @@
+const errors = require("sails-hook-sockets/lib/errors");
+
+
+
+module.exports ={
+
+async create(req,res){
+    try{
+let params= req.allParams();
+
+if(!params.name){
+    return res.badRequest({err: "the name is required"})}
+
+else{
+
+const results= await Company.create({
+
+name: params.name,
+brief:params.brief,
+desc:params.desc,
+img:params.img,
+
+});
+return res.ok(results);
+}
+}catch(err){
+    return res.serverError(err);
+}
+
+
+}
+
+
+
+   ,
+async find(req,res){
+try{
+const companies= await Company.find();
+
+return res.ok(companies);
+}catch(err){
+    return res.serverError(err);
+}
+
+}
+,
+
+
+async findOne(req,res){
+    try{
+
+const MyCompany= await Company.findOne({
+
+    id: req.params.id
+   
+});
+
+
+return res.ok(MyCompany);
+
+
+    }catch(err){
+return res.serverError(err);
+    }
+
+}
+,
+
+
+
+
+}
---
+++ api/models/Company.js
@@ -0,0 +1,42 @@
+module.exports ={
+
+
+attributes:{
+
+name:{ 
+    
+    type:'string',
+    required:true
+
+},
+
+brief: {
+
+    type:'string',
+
+
+}
+,
+
+desc: {
+
+    type:'string',
+
+
+},
+
+img: {
+
+    type:'string',
+
+
+},
+
+
+
+
+
+}
+
+
+}
